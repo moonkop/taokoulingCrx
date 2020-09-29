40d24cbbebfe756beec63b3653fbc1d150682284
moonkop <moonkop@qq.com>
2020-09-29 15:55:47 +0800

    rename index.html to popup.html update gitignore to ignore zip

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/popup.html'
         }),
         new copyPlugin({
             patterns: [
